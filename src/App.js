a6245fc96d35d07b7a4199eece7f356f83855f92
João Vitor Campos Leitão <jvcampos130@gmail.com>
2024-08-27 22:28:54 -0400

    testepage

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import MainPage from './components/MainPage';
 
-function BasicExample() {
+function App() {
   return (
-    <Navbar expand="lg" className="custom-navbar">
-      <Container>
-        <Navbar.Brand href="#home">Explora Manaus</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Router>
+      <Routes>
+        <Route exact path="/" component={<HomePage/>} />
+        <Route path="/main" component={<MainPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
-export default BasicExample;
+export default App;
