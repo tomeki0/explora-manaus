1c602c47d3b7ab2eca6c6c35d0b0c27b78dffb55
João Vitor Campos Leitão <jvcampos130@gmail.com>
2024-08-29 17:59:05 -0400

    mudança na home agora vai

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,42 +1,74 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../components/homepage.css';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/card';
+import Form from 'react-bootstrap/Form';
 
 function HomePage() {
+  const moreInfoRef = useRef(null);
   const navigate = useNavigate();
 
-  const handleEnterClick = () => {
-    navigate('/main');
+  const handleScrollClick = () => {
+    if (moreInfoRef.current) {
+      moreInfoRef.current.style.display = 'block';
+      moreInfoRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
   };
 
   return (
-      <div className='home-page'>
-        <div className='logo-container'>
-          <img src={require('../img/logo.png')} alt='Logo Explora Manaus' className='logo'/>
+    <div className="home-page">
+      <div className="top-section">
+        <div className="left-container">
+          <div className="logo-container">
+            <img src={require('../img/logo.png')} alt="Logo Explora Manaus" className="logo" />
+          </div>
+          <div className="welcome-container">
+            <h1 className="display-3 mb-4 animated-text">
+              Bem vindo(a) ao Explora Manaus!!
+            </h1>
+            <p className='site-description'>
+              Explora Manaus é uma plataforma que permite usuários compartilharem eventos, locais e tudo o que acontece na cidade de Manaus.
+            </p>
+            <Button onClick={handleScrollClick} className="custom-button-transparent">
+              Saber Mais
+            </Button>
+          </div>
         </div>
-      <div className='content-container'>
-        <div className='welcome-container'>
-          <h1 className='display-3 mb-4 animated-text'>
-            Bem vindo(a) ao Explora Manaus!!
-          </h1>
-          <Button onClick={handleEnterClick} className='custom-button' size='lg'>
-            Entrar
-          </Button>
+        
+        <div className="right-container">
+          <div className='overlay'>
+            <Form className="login-form">
+              <Form.Group controlId='formBasicEmail'>
+                <Form.Label>Email</Form.Label>
+                <Form.Control type='email' placeholder='Insira seu email...' />
+              </Form.Group>
+
+              <Form.Group controlId='formBasicPassword' className='mt-3'>
+                <Form.Label>Senha</Form.Label>
+                <Form.Control type='password' placeholder='Insira sua senha...' />
+              </Form.Group>
+              
+              <Button variant='primary' type='submit' className="mt-4 custom-button">
+                Login / Criar Conta
+              </Button>
+
+              <Button onClick={() => navigate('/main')} variant="secondary" className='nt-2 custom-button'>
+                Entrar sem conta
+              </Button>
+            </Form>
+          </div>
         </div>
-        <Card className='info-card'>
-        <Card.Header className="card-header">
-          <Card.Title>Explora Manaus</Card.Title>
-          </Card.Header>
-          <Card.Img variant="top" src={require('../img/card-manaus.png')} alt="Mapa de Manaus"/>
-         <Card.Body>
-            <Card.Text>
-              Este site é uma ferramenta que ajuda os usuarios a compartilhar locais, eventos, shows e tudo que acontece na cidade de manaus
-            </Card.Text>
-          </Card.Body>
-        </Card>
+      </div>
+
+      <div className='more-info-section' ref={moreInfoRef}>
+        <h2 className='info-title'>Sobre o Site</h2>
+        <p className='info-content'>
+          Manaus é a capital do estado do Amazonas, localizada no coração da floresta amazônica.
+          Conhecida por sua rica cultura, biodiversidade e eventos históricos, é um ponto turístico 
+          e econômico crucial na região Norte do Brasil. Explore os melhores pontos da cidade e conheça 
+          mais sobre seus eventos, cultura e locais icônicos através de nossa plataforma.
+        </p>
       </div>
     </div>
   );
