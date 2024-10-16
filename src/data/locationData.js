5ceaf4f67a1661d3d82e0f2275fede85d76e658f
Guilherme Lima <theguilhermelimas@gmail.com>
2024-10-16 16:41:25 -0400

    novos locais

--- src/data/locationData.js
+++ src/data/locationData.js
@@ -1,19 +1,35 @@
-// locationsData.js
-
+// src/data/locationsData.js
 const locationsData = [
     {
-        name: 'Museu da Amazônia',
-        description: 'Um museu incrível dentro da floresta amazônica.',
-        category: 'Cultura e Entretenimento',
-        coordinates: [-60.025778, -3.10719]
+        name: 'Restaurante do Chef',
+        description: 'Culinária internacional com pratos exclusivos.',
+        category: 'Restaurantes e Bares',
+        coordinates: [-60.0456, -3.1024]
     },
     {
-        name: 'Praia de Ponta Negra',
-        description: 'Uma praia urbana no coração de Manaus.',
-        category: 'Natureza e Lazer',
-        coordinates: [-60.064947, -3.08702]
+        name: 'Hotel Manaus Palace',
+        description: 'Conforto e elegância no coração da cidade.',
+        category: 'Hotéis e Acomodações',
+        coordinates: [-60.0203, -3.1034]
     },
-    
+    {
+        name: 'Mercado Municipal Adolpho Lisboa',
+        description: 'Um mercado histórico com produtos locais, frutas exóticas e comidas típicas.',
+        category: 'Comércios Locais',
+        coordinates: [-60.021746, -3.117261]
+    },
+    {
+        name: 'Teatro Amazonas',
+        description: 'Um dos mais belos teatros do Brasil, famoso por sua arquitetura e apresentações culturais.',
+        category: 'Entretenimento e Centros Culturais',
+        coordinates: [-60.021731, -3.100012]
+    },
+    {
+        name: 'Centro Cultural dos Povos da Amazônia',
+        description: 'Um espaço dedicado à cultura e à história dos povos amazônicos.',
+        category: 'Entretenimento e Centros Culturais',
+        coordinates: [-60.021884, -3.112963]
+    },
 ];
 
 export default locationsData;
