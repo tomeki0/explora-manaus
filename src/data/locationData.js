d7c6d64dc261ccc8a095c6fa3c4296c3d6a417cd
Guilherme Lima <theguilhermelimas@gmail.com>
2024-10-16 14:46:17 -0400

    criando pastas e arquivos dos locais e eventos

---
+++ src/data/locationData.js
@@ -0,0 +1,17 @@
+// locationsData.js
+
+export const savedLocations = [
+    {
+        name: 'Museu da Amazônia',
+        description: 'Um museu incrível dentro da floresta amazônica.',
+        category: 'Cultura e Entretenimento',
+        coordinates: [-60.025778, -3.10719]
+    },
+    {
+        name: 'Praia de Ponta Negra',
+        description: 'Uma praia urbana no coração de Manaus.',
+        category: 'Natureza e Lazer',
+        coordinates: [-60.064947, -3.08702]
+    },
+    // Adicione mais locais aqui
+];
